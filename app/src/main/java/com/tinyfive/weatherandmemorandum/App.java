2439f700781b811dbf9aab851d90d2e24a4b5b46
wujun <tinyfive911@163.com>
2018-05-14 21:12:28 +0800

    项目框架初始化

---
+++ app/src/main/java/com/tinyfive/weatherandmemorandum/App.java
@@ -0,0 +1,24 @@
+package com.tinyfive.weatherandmemorandum;
+
+import android.app.Application;
+
+/**
+ * Created by wj on 2018/5/14.
+ */
+
+public class App extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Activity 管理器初始化
+
+        //Network 管理器初始化
+
+        //Picture 管理器初始化
+
+        //数据库初始化
+
+    }
+}
---
+++ app/src/main/java/com/tinyfive/weatherandmemorandum/ui/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.tinyfive.weatherandmemorandum.ui;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wj on 2018/5/14.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
